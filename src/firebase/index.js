444c49374c8647ca7bf8f8ade56f21b61795a681
quocdat2806 <123710745+quocdat2806@users.noreply.github.com>
2024-01-16 08:05:36 +0700

    config : config firebase for server

---
+++ src/firebase/index.js
@@ -0,0 +1,9 @@
+var admin = require("firebase-admin");
+var serviceAccount = require("../../instagram-7c094-firebase-adminsdk-1elos-0b28bbe672.json");
+const { getMessaging } = require("firebase-admin/messaging");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  projectId: "instagram-7c094",
+});
+
+module.exports = { getMessaging };
